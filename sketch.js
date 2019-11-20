528613e6304fede15e18b10b676b935f6670bab7
dario-cfpt <dario.gng@eduge.ch>
2019-11-20 13:51:19 +0100

    Add text with some effect

--- sketch.js
+++ sketch.js
@@ -1,7 +1,30 @@
+const logo = "CFPT-I";
+// color constants
+const backgroundColor = "#0000FF";
+const textColor = "#ff0000";
+// coordinates constants
+const canvasX = 500;
+const canvasY = 500;
+const maxTextSize = 128;
+const startXposition = 0;
+const startYposition = canvasY / 2 + maxTextSize / 2; // try to center the text position
+
+let actualTextSize = maxTextSize;
+let actualXposition = startXposition;
+let actualYposition = startYposition;
+
 function setup() {
-  // put setup code here
+    createCanvas(canvasX, canvasY);
+    textFont('Georgia');
+    fill(textColor);
 }
 
 function draw() {
-  // put drawing code here
+    clear();
+    background(backgroundColor);
+    textSize(actualTextSize);
+    text(logo, actualXposition, actualYposition);
+    if (actualTextSize >= 15) {
+        actualTextSize--;
+    }
 }
